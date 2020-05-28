9f49cd573122304acda860a9c8cdc2aedffcd3b9
Suraj <suraj.rathi00@gmail.com>
2020-05-28 13:24:20 +0530

    Described in slack.
    Fixed some move_base issues, script to publish waypoints. Small launch file changes.

---
+++ scripts/send_waypoints.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python
+
+import rospy
+from rospy import loginfo as INFO
+from rospy import logerr as ERR
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+waypoints = [[4, -21], [5.5, 20], [0, 8.5], [-6, 19]]
+
+
+def main():
+    INFO("Connecting to move_base")
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    client.wait_for_server()
+    INFO("Connected")
+
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.pose.orientation.w = 1.0
+
+    for waypoint in waypoints:
+        INFO("Going to %s." % waypoint)
+
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x, goal.target_pose.pose.position.y = waypoint
+
+        client.send_goal(goal)
+        # if not (wait := client.wait_for_result()):
+        wait = client.wait_for_result()
+        if not wait:
+            INFO("FAIL")
+            rospy.signal_shutdown("Goal not accomplished")
+
+        INFO("Reached.")
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('waypoint_sender')
+        main()
+    except rospy.ROSInterruptException:
+        ERR("FAILED")
